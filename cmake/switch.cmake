06c1033c0cc9c83a4de0483387709dcdbf50e643
h0neybadger <h0neyb4dgerdc@gmail.com>
2020-08-13 17:26:30 +0200

    Attempt to build on devkitpro's container with cmake 3.7

--- cmake/switch.cmake
+++ cmake/switch.cmake
@@ -1,13 +1,101 @@
 # Find DEVKITPRO
-if(NOT DEFINED ENV{DEVKITPRO} OR NOT DEFINED ENV{PORTLIBS_PREFIX})
-    message(FATAL_ERROR "Please set DEVKITPRO & PORTLIBS_PREFIX env before calling cmake. https://devkitpro.org/wiki/Getting_Started")
+if(NOT DEFINED ENV{DEVKITPRO})
+	message(FATAL_ERROR "Please set DEVKITPRO env var before calling cmake. https://devkitpro.org/wiki/Getting_Started")
 endif()
 
 set(DEVKITPRO "$ENV{DEVKITPRO}")
-set(PORTLIBS_PREFIX "$ENV{PORTLIBS_PREFIX}")
 
-# include devkitpro toolchain
-include("${DEVKITPRO}/switch.cmake")
+if(NOT DEFINED ENV{PORTLIBS_PREFIX})
+	# gess portlib prefix if not defined
+	set(PORTLIBS_PREFIX "${DEVKITPRO}/portlibs/switch")
+endif()
+
+
+# /!\ disclaimer
+# we are copying devkitpro's .cmake files (as last resort)
+# since they are missing from official containers.
+# Currently there is no proper way to use pacman durring CI. 
+# https://devkitpro.org/viewtopic.php?f=15&t=9103#p16883
+# downloads.devkitpro.org is 403 forbidden from github CI
+
+# cmake sources: 
+# * https://github.com/devkitPro/pacman-packages/tree/master/pkgbuild-scripts
+# maybe cmake toolchain might be integrated in a futur release
+# * https://github.com/devkitPro/docker
+
+##########################################
+# Copy of /opt/devkitpro/devkita64.cmake #
+##########################################
+cmake_minimum_required(VERSION 3.2)
+
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_PROCESSOR "aarch64")
+set(CMAKE_CROSSCOMPILING 1)
+
+# set(DEVKITPRO /opt/devkitpro)
+
+set(TOOL_PREFIX ${DEVKITPRO}/devkitA64/bin/aarch64-none-elf-)
+
+set(CMAKE_ASM_COMPILER ${TOOL_PREFIX}gcc    )
+set(CMAKE_C_COMPILER   ${TOOL_PREFIX}gcc    )
+set(CMAKE_CXX_COMPILER ${TOOL_PREFIX}g++    )
+set(CMAKE_LINKER       ${TOOL_PREFIX}g++    )
+set(CMAKE_AR           ${TOOL_PREFIX}ar     )
+set(CMAKE_STRIP        ${TOOL_PREFIX}strip  )
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+SET(BUILD_SHARED_LIBS OFF CACHE INTERNAL "Shared libs not available" )
+
+##########################################
+# Copy of /opt/devkitpro/switch.cmake    #
+##########################################
+# include(/opt/devkitpro/devkita64.cmake)
+
+set (DKA_SWITCH_C_FLAGS "-D__SWITCH__ -march=armv8-a+crc+crypto -mtune=cortex-a57 -mtp=soft -ftls-model=local-exec -ffunction-sections -fdata-sections")
+set(CMAKE_C_FLAGS   "${DKA_SWITCH_C_FLAGS}" CACHE STRING "")
+set(CMAKE_CXX_FLAGS "${DKA_SWITCH_C_FLAGS}" CACHE STRING "")
+set(CMAKE_ASM_FLAGS "${DKA_SWITCH_C_FLAGS}" CACHE STRING "")
+
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -specs=${DEVKITPRO}/libnx/switch.specs" CACHE STRING "")
+
+set(CMAKE_FIND_ROOT_PATH
+  ${DEVKITPRO}/devkitA64
+  ${DEVKITPRO}/tools
+  ${DEVKITPRO}/portlibs/switch
+  ${DEVKITPRO}/libnx
+)
+
+# Set pkg-config for the same
+find_program(PKG_CONFIG_EXECUTABLE NAMES aarch64-none-elf-pkg-config HINTS "${DEVKITPRO}/portlibs/switch/bin")
+if (NOT PKG_CONFIG_EXECUTABLE)
+   message(WARNING "Could not find aarch64-none-elf-pkg-config: try installing switch-pkg-config")
+endif()
+
+set(NSWITCH TRUE)
+
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+
+set(NX_ROOT ${DEVKITPRO}/libnx)
+
+set(NX_STANDARD_LIBRARIES "${NX_ROOT}/lib/libnx.a")
+set(CMAKE_C_STANDARD_LIBRARIES "${NX_STANDARD_LIBRARIES}" CACHE STRING "")
+set(CMAKE_CXX_STANDARD_LIBRARIES "${NX_STANDARD_LIBRARIES}" CACHE STRING "")
+set(CMAKE_ASM_STANDARD_LIBRARIES "${NX_STANDARD_LIBRARIES}" CACHE STRING "")
+
+#for some reason cmake (3.14.3) doesn't appreciate having \" here
+set(NX_STANDARD_INCLUDE_DIRECTORIES "${NX_ROOT}/include")
+set(CMAKE_C_STANDARD_INCLUDE_DIRECTORIES "${NX_STANDARD_INCLUDE_DIRECTORIES}" CACHE STRING "")
+set(CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES "${NX_STANDARD_INCLUDE_DIRECTORIES}" CACHE STRING "")
+set(CMAKE_ASM_STANDARD_INCLUDE_DIRECTORIES "${NX_STANDARD_INCLUDE_DIRECTORIES}" CACHE STRING "")
+
+#######################
+# Chiaki project only #
+#######################
 
 # Enable gcc -g, to use
 # /opt/devkitpro/devkitA64/bin/aarch64-none-elf-addr2line -e build_switch/switch/chiaki -f -p -C -a 0xCCB5C
@@ -18,11 +106,12 @@ include("${DEVKITPRO}/switch.cmake")
 # FIXME rework this file to use the toolchain only
 # https://github.com/diasurgical/devilutionX/pull/764
 set(ARCH "-march=armv8-a+crc+crypto -mtune=cortex-a57 -mtp=soft -ftls-model=local-exec")
-# set(CMAKE_C_FLAGS "-O2 -ffunction-sections ${ARCH}")
+set(CMAKE_C_FLAGS "-O2 -ffunction-sections ${ARCH}")
 set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -fno-rtti")
 # workaroud force -fPIE to avoid
 # aarch64-none-elf/bin/ld: read-only segment has dynamic relocations
 set(CMAKE_EXE_LINKER_FLAGS "-specs=${DEVKITPRO}/libnx/switch.specs ${ARCH} -fPIE -Wl,-Map,Output.map")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17")
 
 # add portlibs to the list of include dir
 include_directories("${PORTLIBS_PREFIX}/include")
@@ -76,4 +165,5 @@ function(add_nro_target target title author version icon romfs)
         )
         add_custom_target(${target_we}_nro ALL SOURCES ${CMAKE_CURRENT_BINARY_DIR}/${target_we}.nro)
 endfunction()
+
 
--- scripts/switch/build.sh
+++ scripts/switch/build.sh
@@ -2,6 +2,10 @@
 
 set -xveo pipefail
 
+SCRIPTDIR=$(dirname "$0")
+BASEDIR=$(realpath "${SCRIPTDIR}/../../")
+
+
 arg1=$1
 CHIAKI_SWITCH_ENABLE_LINUX="ON"
 build="./build"
@@ -9,20 +13,21 @@ if [ "$arg1" != "linux" ]; then
   CHIAKI_SWITCH_ENABLE_LINUX="OFF"
   # source /opt/devkitpro/switchvars.sh
   # toolchain="${DEVKITPRO}/switch.cmake"
-  toolchain="cmake/switch.cmake"
-  export PORTLIBS_PREFIX="$(${DEVKITPRO}/portlibs_prefix.sh switch)"
+  toolchain="${BASEDIR}/cmake/switch.cmake"
+  # export PORTLIBS_PREFIX="$(${DEVKITPRO}/portlibs_prefix.sh switch)"
   build="./build_switch"
 fi
 
-SCRIPTDIR=$(dirname "$0")
-BASEDIR=$(realpath "${SCRIPTDIR}/../../")
-
 build_chiaki (){
   pushd "${BASEDIR}"
-    #rm -rf ./build
+    # from debian container
+    # cmake does not seems to create the
+    # ${build} directory
+    [ -d "${build}" ] || mkdir "${build}"
 
-    cmake -B "${build}" \
-	  -DCMAKE_TOOLCHAIN_FILE=${toolchain} \
+    pushd "${BASEDIR}/${build}"
+    cmake \
+      -DCMAKE_TOOLCHAIN_FILE="${toolchain}" \
       -DCHIAKI_ENABLE_TESTS=OFF \
       -DCHIAKI_ENABLE_CLI=OFF \
       -DCHIAKI_ENABLE_GUI=OFF \
@@ -30,10 +35,12 @@ build_chiaki (){
       -DCHIAKI_ENABLE_SWITCH=ON \
       -DCHIAKI_SWITCH_ENABLE_LINUX="${CHIAKI_SWITCH_ENABLE_LINUX}" \
       -DCHIAKI_LIB_ENABLE_MBEDTLS=ON \
+      -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+      "${BASEDIR}"
+
       # -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
       # -DCMAKE_FIND_DEBUG_MODE=ON
 
-    pushd "${BASEDIR}/${build}"
       make -j8
     popd
   popd
